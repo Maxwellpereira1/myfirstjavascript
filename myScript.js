9ff871f175af94a7a00336feee704be9214fc35b
Maxwellpereira1 <pereira.maxwell49@gmail.com>
2018-12-18 08:23:50 -0700

    added varriable stuff

--- myScript.js
+++ myScript.js
@@ -1,4 +1,18 @@
+//this function changes the text in the demo paragraph 
+
 function myFunction () {
-    document.getElementById('demo').innerHTML = "paragraph changed";
+    document.getElementById('demo').innerHTML = "paragraph changed";  
+}
+
+//Defining variables
+
+var x= 5;
+var y= 6;
+var z= x + y;
+
+//This function changes the text in the math paragraph
+
+function mathFunction () {
+    document.getElementById('math').innerHTML = z;
     
 }
