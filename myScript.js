d49c4f354d8639a278578f2d5548ad113746b3c5
Maxwellpereira1 <pereira.maxwell49@gmail.com>
2019-01-09 08:12:21 -0700

    fixed code

--- myScript.js
+++ myScript.js
@@ -1,18 +1,17 @@
 //this function changes the text in the demo paragraph 
 
-function myFunction () {
+function myFunction() {
     document.getElementById('demo').innerHTML = "paragraph changed";  
 }
 
 //Defining variables
 
-var x= 5;
-var y= 6;
-var z= x + y;
+
+
 
 //This function changes the text in the math paragraph
 
-function mathFunction () {
-    document.getElementById('math').innerHTML = z;
-    
+function mathFunction(x,y) {
+    return x + y;
 }
+    
